3c28867461400e231b86fde76b96328fce9d8831
xiaokaixuan <kaixuan1115@126.com>
2018-02-07 17:00:07 +0800

    Add node_v8.0.0-alpine

---
+++ node_v8.0.0-alpine/server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+/usr/bin/redis-server /etc/redis.conf
+
+/usr/bin/mongod --fork --bind_ip 0.0.0.0 --dbpath /var/lib/mongodb --logpath /var/log/mongodb/mongodb.log
+
+/usr/bin/redis-cli flushall
+
+cd /root/gamepath
+
+[ ! -e /root/.npm_rebuild -a -e node_modules ] && npm rebuild --loglevel warn && touch /root/.npm_rebuild
+
+[ -e node_modules/.bin/memdbcluster ] && (node_modules/.bin/memdbcluster start -c config/memdb.conf.js || exit $?)
+
+sleep 1 && [ -e node_modules/.bin/pomelo ] && node_modules/.bin/pomelo start "$@"
+
