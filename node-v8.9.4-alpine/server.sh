b6e5dc0fe90b907bd38adecf11b1acf6dc1de838
xiaokaixuan <kaixuan1115@126.com>
2018-05-22 19:39:12 +0800

    bug-fix: server.sh

--- node-v8.9.4-alpine/server.sh
+++ node-v8.9.4-alpine/server.sh
@@ -10,7 +10,7 @@ cd /root/gamepath
 
 [ ! -e /root/.npm_rebuild -a -e node_modules ] && npm rebuild --loglevel warn && touch /root/.npm_rebuild
 
-[ -e node_modules/.bin/memdbcluster ] && (node_modules/.bin/memdbcluster start -c config/memdb.conf.js || exit $?)
+[ -e node_modules/.bin/memdbcluster ] && { node_modules/.bin/memdbcluster start -c config/memdb.conf.js || exit $?; }
 
 sleep 1 && [ -e node_modules/.bin/pomelo ] && node_modules/.bin/pomelo start "$@"
 
