618d4644de1ccd9393d1406a7ec3fe87f523d1f1
xiaokaixuan <kaixuan1115@126.com>
2018-06-13 10:49:43 +0800

    Optimize script

--- node-v8.9.4-alpine/server.sh
+++ node-v8.9.4-alpine/server.sh
@@ -4,8 +4,6 @@
 
 /usr/bin/mongod --quiet --bind_ip 0.0.0.0 --dbpath /var/lib/mongodb --logpath /var/log/mongodb/mongod.log &
 
-PID1=`pgrep redis-server` PID2=`pgrep mongod`
-
 sleep 1 && /usr/bin/redis-cli flushall
 
 cd /root/gamepath
@@ -18,7 +16,7 @@ sleep 1 && [ -e node_modules/.bin/pomelo ] && node_modules/.bin/pomelo start "$@
 
 PID0=$!
 
-PIDS=`pgrep node`
+PIDS=`pgrep node` PID1=`pgrep redis-server` PID2=`pgrep mongod`
 
 trap '[ -e node_modules/.bin/memdbcluster ] && node_modules/.bin/memdbcluster stop -c config/memdb.conf.js; [ "$PIDS" -o "$PID1" -o "$PID2" ] && kill $PIDS $PID1 $PID2 2>/dev/null' SIGINT SIGTERM
 
