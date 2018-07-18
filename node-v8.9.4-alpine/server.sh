3f1853d8374a0866e15d5cd1d2278f2065b5b3b6
xiaokaixuan <kaixuan1115@126.com>
2018-07-18 10:59:34 +0800

    Mod server.sh

--- node-v8.9.4-alpine/server.sh
+++ node-v8.9.4-alpine/server.sh
@@ -16,9 +16,9 @@ sleep 1 && [ -e node_modules/.bin/pomelo ] && node_modules/.bin/pomelo start "$@
 
 PID0=$!
 
-PIDS=`pgrep node` PID1=`pgrep redis-server` PID2=`pgrep mongod`
+PID1=`pgrep redis-server` PID2=`pgrep mongod`
 
-trap '[ -e node_modules/.bin/memdbcluster ] && node_modules/.bin/memdbcluster stop -c config/memdb.conf.js; [ "$PIDS" -o "$PID1" -o "$PID2" ] && kill $PIDS $PID1 $PID2 2>/dev/null' SIGINT SIGTERM
+trap 'kill $(pgrep -f app.js); [ -e node_modules/.bin/memdbcluster ] && node_modules/.bin/memdbcluster stop -c config/memdb.conf.js; [ "$PID1" -o "$PID2" ] && kill $PID1 $PID2' SIGINT SIGTERM
 
 wait $PID0
 wait $PID1
