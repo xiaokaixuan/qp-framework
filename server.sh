47c7742fcfcad9067df1eb7a60eb6673f003f6e0
xiaokaixuan <xiaokaixuan@gmail.com>
2018-10-10 11:48:38 +0800

    Depart memdb

--- server.sh
+++ server.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-/usr/bin/runsvdir /root/service &
+[ $DB_SERVER ] && { /usr/bin/runsvdir /root/service & }
 
 PID1=$!
 
 sleep 1 && cd /root/gamepath
 
-[ ! -e /root/.npm_rebuild -a -e node_modules ] && npm rebuild && touch /root/.npm_rebuild
+[ ! -e /root/.npm_rebuild -a -e node_modules ] && npm rebuild && touch /root/.npm_rebuild && exit 0
 
-[ -e node_modules/.bin/memdbcluster ] && { node_modules/.bin/memdbcluster start -c config/memdb.conf.js || exit $?; }
+[ $DB_SERVER ] && [ -e node_modules/.bin/memdbcluster ] && { node_modules/.bin/memdbcluster start -c config/memdb.conf.js || exit $?; }
 
-sleep 1 && [ -e node_modules/.bin/pomelo ] && node_modules/.bin/pomelo start "$@" &
+sleep 1 && [ -e node_modules/.bin/pomelo ] && { node_modules/.bin/pomelo start "$@" & }
 
 PID0=$!
 
-trap 'kill $(pgrep -f app.js); [ -e node_modules/.bin/memdbcluster ] && node_modules/.bin/memdbcluster stop -c config/memdb.conf.js; [ "$PID1" ] && kill $PID1' SIGINT SIGTERM
+trap 'kill $(pgrep -f app.js); [ $DB_SERVER ] && [ -e node_modules/.bin/memdbcluster ] && node_modules/.bin/memdbcluster stop -c config/memdb.conf.js; [ "$PID1" ] && kill $PID1' SIGINT SIGTERM
 
 wait $PID0
 wait $PID1
